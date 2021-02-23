960c28907c2610a4085efac19cce163cded854e4
Aysegul Demir <aydemgul@hotmail.com>
2021-02-23 16:13:49 +0000

    add images to blogpost

--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -6,6 +6,9 @@ import blogPost from "../../data/blog.json";
 
 
 
+
+
+
 /**
 * @author
 * @function BlogPost
@@ -36,7 +39,7 @@ const BlogPost = (props) => {
 
     if(post.blogImage =="") return null;
 
-
+    
   return(
       
       <div className="blogPost">
@@ -47,7 +50,8 @@ const BlogPost = (props) => {
         <span className="postBy">posted on{post.postedOn} by {post.author}</span>
         </div>
             <div className="blogPostimg">
-            <img src={require("../../blogpostImg/fashion.jpg")} alt="post image"/>  
+           
+            <img src={"/blogpostImg/" + post.blogImage} alt="post image"/>  
             </div>
         <div className="firstPost">
             <h3>{post.blogTitle}</h3>
