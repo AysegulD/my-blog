5dc6058ba8665597ed66b7a8ab70b4a2e6f809b8
Aysegul Demir <aydemgul@hotmail.com>
2021-03-10 12:51:54 +0000

    lastchangecommit

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,50 +2,60 @@
 import React from 'react';
 import "./style.css";
 import Button from "react-bootstrap/Button";
+import SocialNetwork from "../SocialNetwork";
 
 /**
 * @author
 * @function Footer
 **/
 
+
 const Footer = (props) => {
+ 
   const year=new Date().getFullYear()
   return(
     <footer className="footer">
-    <div className="container">
-      <div className="row">
+    <div className="FooterContainer">
+      <div className="row"  >
         <div className="col-md-6" >
-          <h3>Footer Content</h3>
           <ul>
-            <li>
-            <Button variant="primary">Primary</Button>{' '}
-            </li>
+            <Button variant="primary">Get In Touch</Button>{' '} 
           </ul>
         </div>
-        <div className="col" md="6">
-          <h4> Title</h4>
+        <div className="col" md="3">
           <ul>
             <li>
-              <a href="#">Link</a>
+              <a href="#">Home</a>
             </li>
             <li>
-              <a href="#">Link</a>
+              <a href="#">About</a>
             </li>
             <li>
-              <a href="#">Link</a>
+              <a href="#">Post</a>
             </li>
             <li>
-              <a href="#">Link</a>
-            </li>
-            <li>
-              <a href="#">Link</a>
+              <a href="#">Contact</a>
             </li>
           </ul>
+          <div className="col" md="3">
+            <ul>
+            <li>
+              <a href="#">On Instagram</a>
+            </li>
+            <li>
+              <a href="#">On Facebook</a>
+            </li>
+            <li>
+              <a href="#">AYDEMGUL@HOTMAIL.COM</a>
+            </li>
+            </ul>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
    
-    <div>
+   
+    <div className="year">
      <p className="footerP"> Copyright ⓒ {year} AD </p>
     </div>
     </footer>
